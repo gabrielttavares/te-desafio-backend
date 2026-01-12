173e1e0219e35b8c29e0d8b9c1f7fbd60753fb57
gabrielttavares <gabrieltxtavares@gmail.com>
2026-01-11 21:38:53 -0300

    Initial commit, project init

---
+++ src/main/java/com/projetinho/te_desafio/TeDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.projetinho.te_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetinho/te_desafio/TeDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetinho.te_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
