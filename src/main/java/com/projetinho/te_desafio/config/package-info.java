c8b3bc6c25d77e0597d56c20d4b2fc2cbc0bf9d3
gabrielttavares <gabrieltxtavares@gmail.com>
2026-01-24 23:31:57 -0300

    Remove old structure; add flowchart documentation for backend processes.

---
+++ src/main/java/com/projetinho/te_desafio/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Config: CORS, beans gerais (nao Security).
+ */
+package com.projetinho.te_desafio.config;
--- src/main/java/infrastructure/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package infrastructure.repository;
-
-import infrastructure.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
